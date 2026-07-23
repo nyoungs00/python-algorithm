36c338479c9a44989ce5463d743b08def79d8c87
nyoungs00 <qhgus7888@naver.com>
2026-07-23 12:46:40 +0900

    [프로그래머스] [PCCE 모의고사] 2번 풀이 제출
    - 문제 ID: 240607
    - 난이도: unknown
    - 제출일: 2026. 07. 23. 12:46:36
    - 작성자: 조현영

---
+++ programmers/other/240607_[PCCE_모의고사]_2번.py
@@ -0,0 +1,11 @@
+# [PCCE 모의고사] 2번
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/240607
+# 작성자: 조현영
+# 작성일: 2026. 07. 23. 12:46:36
+
+n = int(input())
+
+num_diagonal = n * (n - 3) / 2
+
+print(int(num_diagonal))
