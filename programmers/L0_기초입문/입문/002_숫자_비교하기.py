8269ea167cc029ed5f57157b7c43800f1b611acf
nyoungs00 <qhgus7888@naver.com>
2026-07-30 19:17:36 +0900

    [프로그래머스] L0 - 숫자 비교하기 (#2)

--- programmers/L0_기초입문/입문/002_숫자_비교하기.py
+++ programmers/L0_기초입문/입문/002_숫자_비교하기.py
@@ -2,8 +2,8 @@
 # 프로그래머스 L0 (기초·입문)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120807
 # 알고리즘: 기초
-# 작성자: 조현영
-# 작성일: 2026. 07. 18. 13:30:41
+# 작성자: 학생
+# 작성일: 2026. 07. 30. 19:17:28
 
 def solution(num1, num2):
     if num1 == num2 :
