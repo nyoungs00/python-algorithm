4a9dfcbce1f11998f076ce823a1b2f83211c58bd
nyoungs00 <qhgus7888@naver.com>
2026-08-04 20:54:18 +0900

    [프로그래머스] L0 - 편지 (#15)

---
+++ programmers/L0_기초입문/입문/015_편지.py
@@ -0,0 +1,10 @@
+# 편지
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120898
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 08. 04. 20:54:13
+
+def solution(message):
+    answer = 2*len(message)
+    return answer
