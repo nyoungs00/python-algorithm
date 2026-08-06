5b72d58f60644f2a95162a2430cd869d6e7fd8a3
nyoungs00 <qhgus7888@naver.com>
2026-08-06 20:48:07 +0900

    [프로그래머스] L0 - 머쓱이보다 키 큰 사람 (#19)

---
+++ programmers/L0_기초입문/입문/019_머쓱이보다_키_큰_사람.py
@@ -0,0 +1,14 @@
+# 머쓱이보다 키 큰 사람
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120585
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 08. 06. 20:48:07
+
+def solution(array, height):
+    count= 0
+    for h in array:
+        if h>height:
+            count += 1
+    answer = count
+    return answer
